03bb49a52b0b7e78f02703cd76ec979dc1bb9db4
Shubham Awasthi <shubhamawasthi06@gmail.com>
2020-10-18 21:15:11 +0530

    Final commit. Added Readme file and some minor changes

--- ui/application_data.py
+++ ui/application_data.py
@@ -1,5 +1,4 @@
 """Module to fetch data from UI"""
-import time
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
